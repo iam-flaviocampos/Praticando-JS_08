39369eba65265643e71044f58a79266c54b22baf
Flávio Campos <flaviobatista2022@hotmail.com>
2023-05-24 10:42:58 -0300

    exercicio 08.js

--- exercicio 08.js
+++ exercicio 08.js
@@ -1,5 +1,5 @@
 /*
-Pedro joga N jogos de basquete por temporada. Para saber como está ele está progredindo, ele mantém
+08) Pedro joga N jogos de basquete por temporada. Para saber como está ele está progredindo, ele mantém
 registro de todos os as pontuações feitas por jogo. Após cada jogo ele anota no novo valor e confere se o
 mesmo é maior ou menor que seu melhor e pior desempenho. Dada uma lista string = “pontuação1 pontuação2
 pontuação3 etc..”, escreva uma função que ao recebê-la irá comparar os valores um a um e irá retornar um
